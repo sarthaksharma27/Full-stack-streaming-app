3615e62d6211995dae5ab64c41b3023dd965d4be
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-07-24 05:52:09 +0530

    setup the backend

---
+++ backend/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+const result = add(5, 8);
+console.log(result);
---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+function add (a: number, b: number): number {
+    return a + b;
+}
+
+const result = add(5, 8)
+console.log(result);
