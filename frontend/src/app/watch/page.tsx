99d59fc76c6808b4cb0a5f85fb2407d33db17bec
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-07-27 14:29:38 +0530

    refactor: rm unnesory comments

--- frontend/src/app/watch/page.tsx
+++ frontend/src/app/watch/page.tsx
@@ -1,13 +1,9 @@
-// app/watch/page.tsx
-
-import HlsPlayerLoader from '@/components/HlsPlayerLoader'; // Import the new loader
+import HlsPlayerLoader from '@/components/HlsPlayerLoader';
 
 export default function WatchPage() {
   return (
     <main style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Watch Live</h1>
       <div style={{ maxWidth: '900px', margin: 'auto' }}>
-        {/* Use the loader component here */}
         <HlsPlayerLoader />
       </div>
     </main>
